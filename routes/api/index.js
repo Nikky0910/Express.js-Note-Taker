e6e37c00ab829798bc40c4e7afb5cb9354ae3435
Marianna <mariannaniko1092@gmail.com>
2025-01-01 19:12:53 -0500

    Update index.js and notes.js files in the api folder

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const noteRoutes = require("./notes");
+
+// http:localhost:3001/api/notes
+router.use("/notes", noteRoutes);
+
+
+
+
+module.exports = router;
